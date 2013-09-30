21ef0d0623c875222388979481112b0f0277180d
stevethibault <steve@patternsoft.net>
2013-09-30 07:47:08 -0400

    Reading a bitmap 32 bit r8g8b8a8

--- image/image.c
+++ image/image.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
+#include <memory.h>
+#include <stdlib.h>
 #include "image.h"
 
 void image_load_bmp(char *filename)
 {
     FILE *f;
-    BITMAPFILEINFOHEADER fileInfoHeader;
-    char type[3];
-
+    BITMAPFILEINFOHEADER file_info_header;
+    BITMAPIMAGEHADER image_header;
+    char tag[3];
+    image im;
+    memset(tag, 0, 3 * sizeof(char));
+    memset(&file_info_header, 0, sizeof(BITMAPFILEINFOHEADER));
 
     f = fopen(filename, "rb");
     if (!f)
@@ -14,15 +19,42 @@ void image_load_bmp(char *filename)
         fprintf(stderr, "Could not open %s\n", filename);
     }
 
-    fgets(type, 2, f);
-    type[2] = '\0';
+    size_t charsRead = fread(tag, 1, 2, f);
+    printf("Chars read: %zu\n", charsRead);
 
-    if (fread(&fileInfoHeader, sizeof(BITMAPFILEINFOHEADER), 1, f))
+    if (charsRead != 2)
     {
         fprintf(stderr, "Error reading image tag for %s\n", filename);
+        fclose(f);
+        return;
     }
 
-    printf("Type: %s Size: %d Offset: %d", type, fileInfoHeader.size, fileInfoHeader.offbytes);
+    if (fread(&file_info_header, sizeof(BITMAPFILEINFOHEADER), 1, f) != 1)
+    {
+        fprintf(stderr, "Error reading image file header for %s\n", filename);
+        fclose(f);
+        return;
+    }
+
+    if (fread(&image_header, sizeof(BITMAPIMAGEHADER), 1, f) != 1)
+    {
+        fprintf(stderr, "Error reading image header for %s\n", filename);
+        fclose(f);
+        return;
+    }
+
+    printf("Type: %s Size: %d Offset: %d Width: %d Height: %d BPP: %d\n", tag, file_info_header.size, file_info_header.offbytes, image_header.width, image_header.height, image_header.bits_per_pixel);
+
+    fseek(f, file_info_header.offbytes, SEEK_SET);
+
+
+    pixels = (pixel *)malloc((size_t)image_header.width * image_header.height);
+    memset(pixels, 0, image_header.width * image_header.height);
+
+    if (fread(image.data, sizeof(pixel), image_header.width * image_header.height, f) != image_header.width * image_header.height)
+    {
+        fprintf(stderr, "Error reading image pixels %s\n", filename);
+    }
 
     fclose(f);
 }
--- image/image.h
+++ image/image.h
@@ -9,5 +9,34 @@ typedef struct
     int offbytes;
 } BITMAPFILEINFOHEADER;
 
+typedef struct
+{
+    int headerSize;
+    int width;
+    int height;
+    short plane;
+    short bits_per_pixel;
+    int compression_type;
+    int image_size;
+    int x_pixels_per_meter;
+    int y_pixels_per_meter;
+    int used_colors;
+    int important_colors;
+} BITMAPIMAGEHADER;
 
+
+typedef struct
+{
+    unsigned char red; /* Red component */
+    unsigned char green; /* Green component */
+    unsigned char blue; /* Blue component */
+    unsigned char alpha; /* Alpha component */
+} pixel;
+
+typedef struct
+{
+    int width;
+    int height;
+    pixel *data;
+} image;
 #endif // IMAGE_H_INCLUDED
